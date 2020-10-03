c4d0e409192c6b125a0cfeb9b14bf98770246ec0
David Moraes <InvesTraderRS@gmail.com>
2020-10-03 15:02:36 -0300

    Conceito Path

---
+++ ExercicioComArquivos/ExercicioComArquivos/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercicioComArquivos.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Path/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * PATH:
+             - Realiza operações com strings que contém informações de arquivos ou pasta
+             */
+
+            string path = @"C:\ProgramasCSharp\ConceitoDePath\Path\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar); // .DirectorySeparatorChar é uma operação(Método) que utiliza um caracter para a separação. Nesse caso é a baara invertida "\" pois no sistema windows é essa barra invertida
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator); //.PathSeparator é uma operação(Método) que mostra qual o caracter utilizado para separar entre path diferentes. 
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); // .GETDirectoryName é uma operação(Método) que pega apenas o caminho da pasta que contém o arquivo sem incluir seu nome junto
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path)); //.GetFileName é uma função´para pegar o nome do arquivo.
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path)); // .GetExtension é uma função que pega apenas a extenção do arquivo
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path)); // .GetFileNameWithoutExtension é uma funçao que ´pega apenas o nome do arquivo sem sua extenção
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path)); // .GetFullPath pega todo o caminho do arquivo junto com o nome do arquivo e sua extenção.
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath()); // .GetFullPath é uma função que mostra o caminho da pasta que guarda temporariamento dados.
+
+        }
+    }
+}
